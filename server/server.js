2a946ccf33f1c1917f2d9a275975398d91f89d42
pastorcash <cmyers880@gmail.com>
2018-01-19 15:22:46 -0600

    Initial setup

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+// const bodyParser = require('body-parser');
+
+const publicPath = path.join(__dirname, '../public');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+// app.use(bodyParser.json());
+
+// ----- Activate listener ----- //
+app.listen(port, () => {
+    console.log(`Started up at port ${port}`);
+  });
+  
+  module.exports = {app};
